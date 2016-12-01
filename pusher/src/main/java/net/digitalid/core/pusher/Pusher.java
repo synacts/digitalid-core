6a2b872a8156b2cc2c4b87fb7110f45e5e1e2201
Kaspar Etter <kaspar.etter@digitalid.net>
2016-12-01 10:21:56 +0100

    Made the error and pusher projects compile by commenting out their existing but very limited logic.

--- pusher/src/main/java/net/digitalid/core/pusher/Pusher.java
+++ pusher/src/main/java/net/digitalid/core/pusher/Pusher.java
@@ -2,11 +2,14 @@ package net.digitalid.core.pusher;
 
 import javax.annotation.Nonnull;
 
-import net.digitalid.utility.system.logger.Log;
+import net.digitalid.utility.annotations.method.PureWithSideEffects;
+import net.digitalid.utility.logging.Log;
+import net.digitalid.utility.validation.annotations.type.Utility;
 
 import net.digitalid.database.annotations.transaction.NonCommitting;
+import net.digitalid.database.exceptions.DatabaseException;
 
-import net.digitalid.service.core.handler.ExternalAction;
+import net.digitalid.core.handler.method.action.ExternalAction;
 
 /**
  * Pushes the external actions to their recipients (and retries on failure).
@@ -15,9 +18,11 @@ import net.digitalid.service.core.handler.ExternalAction;
  * 
  * @see PushFailed
  */
-public final class Pusher extends Thread {
+@Utility
+public abstract class Pusher extends Thread {
     
     @NonCommitting
+    @PureWithSideEffects
     public static void send(@Nonnull ExternalAction action) throws DatabaseException {
         Log.error("The action '" + action + "' should have been pushed but this is not implemented yet."); // TODO: Write a real implementation!
     }
